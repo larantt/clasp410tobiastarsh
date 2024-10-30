84e3a0fb8a996b7c7aa218980b8085e3a1587296
laratt <laratt@umich.edu>
2024-10-29 20:46:00 -0400

    added bar charts

--- labs/lab04/lab04.py
+++ labs/lab04/lab04.py
@@ -27,13 +27,24 @@ from pathlib import Path
 import unittest
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-
+import datetime
 
 # Kangerlussuaq average temperature:
 T_KANGER = np.array([-19.7, -21.0, -17., -8.4, 2.3, 8.4,
 10.7, 8.5, 3.1, -6.0, -12.0, -16.9])
-OUTPATH = "/Users/laratobias-tarsh/Documents/fa24/clasp410tobiastarsh/labs/lab04/figures_new"
+
+# define nice gradient blue red color pallete (red, blue)
+COLORS = [("#d5b3b3","#96b6f2"),
+          ("#bc8484","#5689ea"),
+          ("#a15858","#1b5ede"),
+          ("#733f3f","#13439f"),
+          ("#452525","#0b285f")]
+
+# define nice colorblind friendly plotting colors
+# source: https://davidmathlogic.com/colorblind/#%23648FFF-%23785EF0-%23DC267F-%23FE6100-%23FFB000
+IBM_CMAP = ["#648FFF","#785EF0","#DC267F","#FE6100","#FFB000"]
+
+OUTPATH = "/Users/laratobias-tarsh/Documents/fa24/clasp410tobiastarsh/labs/lab04/figures"
 
 # initial configuration for debugging (changed dynamically in heatdiff)
 logging.basicConfig(level=logging.WARNING)
@@ -59,7 +70,6 @@ def bold_axes(ax):
 
     for label in ax.get_yticklabels():
         label.set_weight('bold')
-
 
 def default_init(xgrid):
     """
@@ -137,7 +147,7 @@ def apply_bounds(func, **kwargs):
     """
     func(**kwargs)
 
-def heatmap(u_arr,t_arr,d_arr,units=None):
+def heatmap(u_arr,t_arr,d_arr,units=None,ax=None,fig=None,title='Heatmap',diverge=True):
     """
     Function makes heatmap plot to display the variation of
     heat with depth and time.
@@ -150,9 +160,29 @@ def heatmap(u_arr,t_arr,d_arr,units=None):
         array of times for the simulation
     d_arr : np.array
         array of depths for the simulation
+    units : str, defaults to seconds
+        Units to convert the time into, assumes seconds as a base unit
+    ax : mpl.Axes, defaults to None
+        which axes to plot on if supplied, otherwise creates axes
+    fig : mpl.Figure, defaults to None
+        which figure to plot on if supplied, otherwise creates figure
+    title : str, defaults to 'Heatmap'
+        title for the axis
+    diverge : bool, defaults to True
+        whether to set a diverging colormap over the maximum range of values
+        centered at 0 or not.
+
+    Returns
+    -------
+    fig : mpl.Figure
+        figure with the plot
+    ax : mpl.Axes
+        axes with the plot
+
     """
-    # create figure and axes
-    fig, ax = plt.subplots(1,1,figsize=(12,8))
+    # create figure and axes if they arent already passed
+    if ax is None:
+        fig, ax = plt.subplots(1,1,figsize=(12,8))
 
     # convert times to desired units
     if units == 'days':
@@ -168,56 +198,19 @@ def heatmap(u_arr,t_arr,d_arr,units=None):
     # determine the largest absolute value of the min and max to the nearest 5
     scale_range = 5 * ((np.max([np.abs(u_arr.min()),np.abs(u_arr.max())]) + 4) // 5)
     # make p-color plot
-    cmap = ax.pcolor(t_arr_scaled,d_arr,u_arr,cmap='seismic',vmin=-1*scale_range,vmax=scale_range)
+    if diverge:
+        cmap = ax.pcolor(t_arr_scaled,d_arr,u_arr,cmap='seismic',vmin=-1*scale_range,vmax=scale_range)
+    else:
+        cmap = ax.pcolor(t_arr_scaled,d_arr,u_arr,cmap='cividis_r')
     # set colorbar
     cb = fig.colorbar(cmap, ax=ax, label='Temperature ($C$)')
     # flip axes to decrease depth with height
     ax.invert_yaxis()
-    ax.set_ylabel('depth (m)')
-    ax.set_xlabel(f'time ({units})')
+    ax.set_ylabel('depth (m)',fontweight='bold',fontsize=12)
+    ax.set_xlabel(f'time ({units})',fontweight='bold',fontsize=12)
     bold_axes(ax)
+    ax.set_title(title,fontweight='bold',fontsize=16,loc='left')
 
-    return fig, ax
-
-def temp_profile(u_arr,dt,d_arr):
-    """
-    Function creates a seasonal temperature profile with depth for winter and
-    summer.
-
-    Parameters
-    ----------
-    u_arr : np.ndarray
-        2D array containing the heat equation output
-    dt : float
-        timestep used for indexing the final year
-        as for all times in lab, must be in seconds
-    d_arr : np.array
-        1d array representing depth in the simulation
-
-    Returns
-    -------
-    """
-    # 1 year in seconds
-    y2s = 365*5*86400
-    # now set indexing to get final year
-    loc = int(-y2s/dt)
-
-    # get summer and winter values
-    winter = u_arr[:, loc:].min(axis=1)
-    summer = u_arr[:, loc:].max(axis=1)
-
-    # plot temperature profile
-    fig, ax = plt.subplots(1,1,figsize=(12,8))
-    ax.plot(summer,d_arr,c='#BF4146',label='Summer',lw=3)
-    ax.plot(winter,d_arr,c='#80A1C2',label='Winter',lw=3)
-    ax.invert_yaxis()
-    ax.set_xlabel(r'Temperature ($^\circ$C)',fontsize=12,fontweight='bold')
-    ax.set_ylabel('Depth (m)',fontsize=12,fontweight='bold')
-    ax.legend(loc='upper right',frameon=False,ncols=2)
-    ax.set_title('Seasonal Temperature Profiles at Kangerlussaq',loc='left',fontweight='bold',fontsize=14)
-    ax.axvline(0,ls='--',c='darkgrey',label=r'0$^\circ$C')
-    bold_axes(ax)
-    
     return fig, ax
 
 def heatdiff(xmax, tmax, dx, dt, bconds=default_bounds, init=default_init, c2=1, debug=0):
@@ -335,6 +328,137 @@ def heatdiff(xmax, tmax, dx, dt, bconds=default_bounds, init=default_init, c2=1,
     # Return grid and result:
     return xgrid, tgrid, U
 
+def plot_all_profiles(us,ds,dt,ax,shift,colors,title='Profile of Permafrost'):
+    """
+    Make nice gradient profile for multiple temperatures
+    """
+    # 1 year in seconds
+    y2s = 365*5*86400
+    # now set indexing to get final year
+    loc = int(-y2s/dt)
+
+    # now loop and plot
+    for u_arr, d_arr,cs,s in zip(us,ds,colors,shift):
+        # get summer and winter values
+        winter = u_arr[:, loc:].min(axis=1)
+        summer = u_arr[:, loc:].max(axis=1)
+        # do plotting
+        ax.plot(summer,d_arr,c=cs[0],label=f'Summer ({s}$^\circ$C warming)',lw=3)
+        ax.plot(winter,d_arr,c=cs[1],label=f'Winter ({s}$^\circ$C warming)',lw=3)
+        ax.set_ylim(0,60)  # change later to actually find the bottom of the coldest profile
+        ax.invert_yaxis()
+        ax.set_xlabel(r'Temperature ($^\circ$C)',fontsize=12,fontweight='bold')
+        ax.set_ylabel('Depth (m)',fontsize=12,fontweight='bold')
+        ax.legend(loc='lower left',frameon=False,ncols=2)
+
+    ax.axvline(0,ls='--',c='lightgrey',label=r'0$^\circ$C')
+    ax.set_title(title,loc='left',fontsize=14,fontweight='bold')
+    bold_axes(ax)
+
+def plot_depth_thickness(us,ds,dt,ax,shift,colors,title='Thickness and Depth of Permafrost'):
+    """
+    Function makes a bar plot of depth and thickness of permafrost
+    """
+    # 1 year in seconds
+    y2s = 365*5*86400
+    # now set indexing to get final year
+    loc = int(-y2s/dt)
+
+    # make an empty dictionary to store output
+    perm_data = {}
+    # now loop and plot
+    for u_arr, d_arr,s in zip(us,ds,shift):
+        # get summer and winter values
+        summer = u_arr[:, loc:].max(axis=1)
+        # determine the depth (maximum depth that isothermal zone = 0)
+        # last instance of 0 in the curve before geothermal heating
+        depth_idx = np.argwhere(summer <= 0.)[-1][0]   # note, second index ensures the depth index is retrived
+        depth = d_arr[depth_idx]
+        # determine the thickness (depth of active layer - depth)
+        # first index below 0 in summer will define active layer
+        active_idx = np.argwhere(summer <= 0)[0][0] # note, second index ensures the depth index is retrived
+        thickness = d_arr[depth_idx] - d_arr[active_idx]
+       
+        perm_data[f'+{s} warming'] = (depth,thickness)
+
+    # now plotting
+    labels = ['depth (m)','thickness (m)']
+    x = np.arange(len(labels))  # the label locations
+    width = 0.25  # the width of the bars
+    multiplier = 0
+
+    for lab,measurement in perm_data.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=lab)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+    #ax.set_xticks(i+width,['Depth','Thickness']) 
+    ax.legend(loc='lower left',frameon=False,ncols=2)
+
+    ax.set_title(title,loc='left',fontsize=14,fontweight='bold')
+    bold_axes(ax)
+
+
+
+def permafrost_solve(dx,depth,dt,years,c,shift=3.):
+    """
+    Function runs the same solver but adding a uniform shift
+    to the temperatures (in degrees C)
+
+    Parameters
+    ----------
+    temps : callable
+        Function to determine the temperatures, defaults to 
+        kanger_bounds
+    shift : float
+        uniform shift to add to the initial conditions
+    """
+    # define bounds function to allow shift
+    def shift_bounds(u_arr,time_arr):
+       return kanger_bounds(u_arr,time_arr,shift)
+    
+    # run with everything, including debugging to check it works
+    x_kang,t_kang,u_kang = heatdiff(xmax=depth,tmax=years,dx=dx,dt=dt,
+                                    bconds=shift_bounds,c2=c,init=0,debug=0)
+    return x_kang,t_kang,u_kang
+    
+def shift_temps():
+    """
+    solve for all the warming scenarios and then plot heatmaps of
+    individual and combined profiles of temperature.
+
+    This function will produce the plots necessary for every question
+    as there is no point in running redundant code.
+
+    """
+    # depth is 0m to 100m with dx of 1
+    dx    = 0.5             # unit of m
+    depth = 100             # unit of m
+    dt    = 1*86400        # unit of seconds
+    years  = 365*60*86400   # unit of seconds
+    c      = 2.5e-7         # converted value in lab to m^2/s
+    
+    # plot temperature profile
+    fig, ax = plt.subplots(1,2,figsize=(12,8))
+    us,ds = [],[]
+    shifts=[3.,1.,.5,0.]
+    for shift in shifts:
+        fig2, ax2 = plt.subplots(1,2,figsize=(12,8))
+        x,t,u = permafrost_solve(dx,depth,dt,years,c,shift)
+        us.append(u)
+        ds.append(x)
+        fig1, _ = heatmap(u,t,x,'years')
+        plot_all_profiles([u],[x],1*86400,ax2[0],[shift],colors=COLORS)
+        plot_depth_thickness([u],[x],1*86400,ax2[1],[shift],colors=IBM_CMAP)
+
+        fig1.savefig(f'{OUTPATH}/heatmap_{shift}.png',dpi=300)
+        fig2.savefig(f'{OUTPATH}/temp_profile_{shift}.png',dpi=300)
+
+    plot_all_profiles(us,ds,1*86400,ax[0],shifts,colors=COLORS[::-1])
+    plot_depth_thickness(us,ds,1*86400,ax2[1],shifts,colors=IBM_CMAP)
+    
+    fig.savefig(f'{OUTPATH}/kang_warmed_heatmap.png',dpi=300)
 
 class TestHeatdiff(unittest.TestCase):
     """
@@ -345,7 +469,17 @@ class TestHeatdiff(unittest.TestCase):
     """
     def test_heateqn(self):
         """
-        Unit test solves for test case given in lab code.
+        Unit test solves for wire test case given in lab code. This has 
+        initial conditions of 4*x - 4*(x**2) and boundary conditions set
+        to 0C.
+
+        Also creates and saves a heatmap of the solver solution and the
+        reference solution to be shown in the lab.
+
+        Parameters
+        ----------
+        self : Self@TestHeatdiff
+            unit test class object
         """
         # Solution to problem 10.3 from fink/matthews as a nested list:
         sol10p3 = [[0.000000, 0.640000, 0.960000, 0.960000, 0.640000, 0.000000],
@@ -364,118 +498,12 @@ class TestHeatdiff(unittest.TestCase):
 
         # solve the heat equation for the test case
         x,t,solver_result = heatdiff(1,0.2,0.2,0.02)
-        # plot heatmap for visualisation, could remove this depending on lab requirements
-        heatmap(solver_result,t,x,units='seconds')
+
         # confirm the coefficients and the temperatures are equal to 1 decimal place
-        np.testing.assert_allclose(solver_result, sol10p3, rtol=1E6)
-
-
-def kanger():
-    """
-    Function just reproduces the figures in the lab for Greenland
-    to ensure that the function is working
-    """
-    # depth is 0m to 100m with dx of 1
-    dx    = 0.5             # unit of m
-    depth = 100             # unit of m
-    dt    = 1*86400       # unit of seconds
-    years  = 365*60*86400    # unit of seconds
-    c      = 2.5e-7         # converted value in lab to m^2/s
-    
-    # run with everything, including debugging to check it works
-    x_kang,t_kang,u_kang = heatdiff(xmax=depth,tmax=years,dx=dx,dt=dt,
-                                    bconds=kanger_bounds,c2=c,init=0,debug=0)
-    fig1, ax1 = heatmap(u_kang,t_kang,x_kang,'years')
-    fig2, ax2 = temp_profile(u_kang,dt,x_kang)
-
-    fig1.savefig(f'{OUTPATH}/kang_test_heatmap.png',dpi=300)
-    fig2.savefig(f'{OUTPATH}/kang_test_profile.png',dpi=300)
-    return x_kang, t_kang, u_kang
-
-def plot_all_profiles(us,ds,dt,ax,shift):
-    """
-    Make nice gradient profile for multiple temperatures
-    """
-    # 1 year in seconds
-    y2s = 365*5*86400
-    # now set indexing to get final year
-    loc = int(-y2s/dt)
-    # define nice gradient blue red color pallete (red, blue)
-    colors = [("#d5b3b3","#96b6f2"),
-              ("#bc8484","#5689ea"),
-              ("#a15858","#1b5ede"),
-              ("#733f3f","#13439f"),
-              ("#452525","#0b285f")]
-    # now loop and plot
-    for u_arr, d_arr,cs,s in zip(us,ds,colors[::-1],shift):
-        # get summer and winter values
-        winter = u_arr[:, loc:].min(axis=1)
-        summer = u_arr[:, loc:].max(axis=1)
-        # do plotting
-        ax.plot(summer,d_arr,c=cs[0],label=f'Summer ({s}$^\circ$C warming)',lw=3)
-        ax.plot(winter,d_arr,c=cs[1],label=f'Winter ({s}$^\circ$C warming)',lw=3)
-        ax.set_ylim(0,60)  # change later to actually find the bottom of the coldest profile
-        ax.invert_yaxis()
-        ax.set_xlabel(r'Temperature ($^\circ$C)',fontsize=12,fontweight='bold')
-        ax.set_ylabel('Depth (m)',fontsize=12,fontweight='bold')
-        ax.legend(loc='lower left',frameon=False,ncols=2)
-
-    ax.axvline(0,ls='--',c='lightgrey',label=r'0$^\circ$C')
-    ax.set_title('Seasonal Profiles of Permafrost at Kangerlussaq Under Warming Scenarios',
-                 loc='left',fontsize=14,fontweight='bold')
-    bold_axes(ax)
-
-
-def warm_earth(shift=3.):
-    """
-    Function runs the same solver but adding a uniform shift
-    to the temperatures (in degrees C)
-
-    Parameters
-    ----------
-    temps : callable
-        Function to determine the temperatures, defaults to 
-        kanger_bounds
-    shift : float
-        uniform shift to add to the initial conditions
-    """
-    # define bounds function to allow shift
-    def shift_bounds(u_arr,time_arr):
-       return kanger_bounds(u_arr,time_arr,shift)
-
-    # depth is 0m to 100m with dx of 1
-    dx    = 0.5             # unit of m
-    depth = 100             # unit of m
-    dt    = 1*86400        # unit of seconds
-    years  = 365*60*86400   # unit of seconds
-    c      = 2.5e-7         # converted value in lab to m^2/s
-    
-    # run with everything, including debugging to check it works
-    x_kang,t_kang,u_kang = heatdiff(xmax=depth,tmax=years,dx=dx,dt=dt,
-                                    bconds=shift_bounds,c2=c,init=0,debug=0)
-    return x_kang,t_kang,u_kang
-    
-def shift_temps():
-    """
-    plot
-    """
-    # plot temperature profile
-    fig, ax = plt.subplots(1,1,figsize=(12,8))
-    us,ds = [],[]
-    shifts=[5.,3.,1.,.5,0.]
-    for shift in shifts:
-        x,t,u = warm_earth(shift)
-        us.append(u)
-        ds.append(x)
-        fig1, _ = heatmap(u,t,x,'years')
-        fig2, _ = temp_profile(u,1*86400,x)
-
-        fig1.savefig(f'{OUTPATH}/heatmap_{shift}.png',dpi=300)
-        fig2.savefig(f'{OUTPATH}/temp_profile_{shift}.png',dpi=300)
-
-    plot_all_profiles(us,ds,1*86400,ax,shifts)
-    
-    fig.savefig(f'{OUTPATH}/kang_warmed_heatmap.png',dpi=300)
+        np.testing.assert_allclose(solver_result, sol10p3, rtol=1E-5)
+        # print the date and time of the unit test for reference
+        now = datetime.datetime.now()
+        print(f'Unit test executed at {now.strftime("%Y-%m-%d %H:%M:%S")}')
 
 def main():
     """
@@ -490,7 +518,6 @@ def main():
 
     Path(OUTPATH).mkdir(parents=True, exist_ok=True)
     shift_temps()
-    #kanger()
 
 # run the main function when calling script
 if __name__ == "__main__":
